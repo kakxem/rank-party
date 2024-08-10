1fa05b78b674babf4b697d0b01dc4e7e993521e7
kakxem <80788785+kakxem@users.noreply.github.com>
2024-08-10 02:58:21 +0200

    feat: show all game data

--- src/components/debug.tsx
+++ src/components/debug.tsx
@@ -3,9 +3,7 @@ import type { Game } from "@/types";
 export const Debug = ({ game }: { game: Game }) => {
   return (
     <div className="absolute bottom-0 right-0 bg-slate-300 p-10">
-      <p>
-        Id {game.id}, Name: {game.name}, State: {game.state.toString()}
-      </p>
+      <p>{JSON.stringify(game, null, 2)}</p>
     </div>
   );
 };
