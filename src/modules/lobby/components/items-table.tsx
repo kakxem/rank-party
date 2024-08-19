ec76340e005418f189ae95c854210dd3f7f5ec41
oyoke <oyokedev@gmail.com>
2024-08-19 22:01:21 +0200

    feat: items table component created

---
+++ src/modules/lobby/components/items-table.tsx
@@ -0,0 +1,129 @@
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Input } from "@/components/ui/input";
+import { gameAtom, wsAtom } from "@/hooks/useGame";
+import type { Item } from "@/types";
+import { Messages } from "@/types";
+import { ColumnDef } from "@tanstack/react-table";
+import { useAtomValue } from "jotai";
+import { ArrowUpDown, Trash2 } from "lucide-react";
+
+export const ItemsTable = () => {
+  const ws = useAtomValue(wsAtom);
+  const game = useAtomValue(gameAtom);
+
+  const handleAddItem = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target as HTMLFormElement);
+    const name = formData.get("name") as string;
+    const link = formData.get("link") as string;
+
+    if (ws && name && link) {
+      ws.send(
+        JSON.stringify({
+          type: Messages.ADD_ITEM,
+          data: { name: name, link: link },
+        }),
+      );
+      (e.target as HTMLFormElement).reset();
+    }
+  };
+
+  const handleDeleteItem = (id: string) => {
+    if (ws) {
+      ws.send(
+        JSON.stringify({
+          type: Messages.DELETE_ITEM,
+          data: { id },
+        }),
+      );
+    }
+  };
+
+  const columns: ColumnDef<Item>[] = [
+    {
+      accessorKey: "name",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Name
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => {
+        const name = row.getValue("name") as string;
+        return <div className="max-w-md truncate">{name}</div>;
+      },
+    },
+    {
+      accessorKey: "link",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Link
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => {
+        const link = row.getValue("link") as string;
+        return (
+          <div className="max-w-xs truncate">
+            <a
+              className="truncate underline"
+              href={link}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {link}
+            </a>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "id",
+      header: "",
+      cell: ({ row }) => {
+        const id = row.getValue("id") as string;
+        return (
+          <div className="flex justify-end">
+            <Button variant="ghost" onClick={() => handleDeleteItem(id)}>
+              <Trash2 className="h-4 w-4" />
+            </Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <section className="h-full w-full p-5">
+      <header>
+        <form className="flex gap-2" onSubmit={handleAddItem}>
+          <Input
+            name="name"
+            className="max-w-xs"
+            placeholder="Item name"
+            autoComplete="off"
+          />
+          <Input name="link" placeholder="Item link" autoComplete="off" />
+          <Button type="submit">Add</Button>
+        </form>
+      </header>
+
+      {/* Table */}
+      <div className="py-5">
+        <DataTable columns={columns} data={game.list} />
+      </div>
+    </section>
+  );
+};
--- src/modules/lobby/index.tsx
+++ src/modules/lobby/index.tsx
@@ -1,43 +1,12 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { gameAtom, wsAtom } from "@/hooks/useGame";
-import { Messages } from "@/types";
+import { gameAtom } from "@/hooks/useGame";
+import { ItemsTable } from "@/modules/lobby/components/items-table";
 import { useAtomValue } from "jotai";
 import { useState } from "react";
 
 export const Lobby = () => {
-  const ws = useAtomValue(wsAtom);
   const game = useAtomValue(gameAtom);
   const [showInactive, setShowInactive] = useState(false);
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.target as HTMLFormElement);
-    const name = formData.get("name") as string;
-    const link = formData.get("link") as string;
-
-    if (ws && name && link) {
-      ws.send(
-        JSON.stringify({
-          type: Messages.ADD_ITEM,
-          data: { name: name, link: link },
-        }),
-      );
-      (e.target as HTMLFormElement).reset();
-    }
-  };
-
-  const handleDeleteItem = (id: string) => {
-    if (ws) {
-      ws.send(
-        JSON.stringify({
-          type: Messages.DELETE_ITEM,
-          data: { id },
-        }),
-      );
-    }
-  };
 
   return (
     <section className="flex flex-1 justify-between gap-3">
@@ -75,60 +44,7 @@ export const Lobby = () => {
       </aside>
 
       <div className="flex flex-1 flex-col justify-between gap-3 rounded-xl bg-slate-100 p-5 dark:bg-slate-900">
-        <section className="h-full w-full p-5">
-          <header>
-            <form className="flex gap-2" onSubmit={handleSubmit}>
-              <Input
-                name="name"
-                className="max-w-xs"
-                placeholder="Item name"
-                autoComplete="off"
-              />
-              <Input name="link" placeholder="Item link" autoComplete="off" />
-              <Button type="submit">Add</Button>
-            </form>
-          </header>
-
-          {/* Table */}
-          <ol>
-            {game.list.map((item) => (
-              <li
-                key={item.id}
-                className="flex h-20 items-center gap-3 rounded-md bg-slate-200 p-2 px-3 dark:bg-slate-800"
-              >
-                <div className="flex rounded-md border-2 p-5 dark:border-slate-700">
-                  {item.name.slice(0, 1).toUpperCase()}
-                </div>
-                <p className="flex flex-1 justify-between">
-                  {item.name}
-                  <span className="text-end">{item.link}</span>
-                </p>
-                <Button onClick={() => handleDeleteItem(item.id)}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={24}
-                    height={24}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="icon icon-tabler icons-tabler-outline icon-tabler-trash"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M4 7l16 0" />
-                    <path d="M10 11l0 6" />
-                    <path d="M14 11l0 6" />
-                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                  </svg>
-                </Button>
-              </li>
-            ))}
-          </ol>
-        </section>
-
+        <ItemsTable />
         <Button>Start</Button>
       </div>
     </section>
