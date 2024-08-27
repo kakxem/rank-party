2e67c46de6d978f25e3246bf420dc9120b15892f
kakxem <80788785+kakxem@users.noreply.github.com>
2024-08-27 20:39:17 +0200

    fix: custom types for @justinribeiro/lite-youtube web component

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+import { LiteYTEmbed } from "@justinribeiro/lite-youtube";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "lite-youtube": React.DetailedHTMLProps<
+        React.HTMLAttributes<LiteYTEmbed>,
+        LiteYTEmbed
+      > & {
+        videoid: string;
+        videotitle: string | undefined;
+      };
+    }
+  }
+}
