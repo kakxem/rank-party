9bfb8196c3c63c6f784dde5f5cab1cbd95717a5c
oyoke <oyokedev@gmail.com>
2024-09-27 00:26:59 +0200

    fix: prevent line breaks and limit whitespace in editable room name

--- src/components/editable-room-name.tsx
+++ src/components/editable-room-name.tsx
@@ -1,23 +1,31 @@
 import { gameAtom, wsAtom } from "@/hooks/useGame";
 import { Messages, Scene } from "@/types";
 import { useAtomValue } from "jotai";
-import { useEffect, useRef } from "react";
+import { KeyboardEvent, useEffect, useRef } from "react";
 
 export const EditableRoomName = () => {
   const game = useAtomValue(gameAtom);
   const ws = useAtomValue(wsAtom);
   const roomNameRef = useRef<HTMLHeadingElement>(null);
 
-  const handleInputChange = () => {
-    const newName = roomNameRef.current?.innerText || "";
+  const cleanText = (text: string) => {
+    // Reemplaza múltiples espacios con uno solo y elimina espacios al inicio y al final
+    return text.replace(/\s+/g, " ").trim();
+  };
 
-    if (ws && game.id) {
-      ws.send(
-        JSON.stringify({
-          type: Messages.UPDATE_ROOM_NAME,
-          data: { roomCode: game.id, newName },
-        }),
-      );
+  const handleInputChange = () => {
+    if (roomNameRef.current) {
+      const newName = cleanText(roomNameRef.current.innerText || "");
+      roomNameRef.current.innerText = newName; // Actualiza el contenido visible
+
+      if (ws && game.id) {
+        ws.send(
+          JSON.stringify({
+            type: Messages.UPDATE_ROOM_NAME,
+            data: { roomCode: game.id, newName },
+          }),
+        );
+      }
     }
   };
 
@@ -27,12 +35,20 @@ export const EditableRoomName = () => {
     }
   }, [game.name]);
 
+  const handleKeyDown = (event: KeyboardEvent<HTMLHeadingElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      roomNameRef.current?.blur(); // Quita el foco del elemento
+    }
+  };
+
   return (
     <h1
       contentEditable={game.scene === Scene.LOBBY}
       ref={roomNameRef}
       suppressContentEditableWarning
       onBlur={handleInputChange}
+      onKeyDown={handleKeyDown}
       className="text-center text-4xl font-semibold"
     >
       {game.name || "Party Rank Group"}
