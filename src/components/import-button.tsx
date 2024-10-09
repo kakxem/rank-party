265c4fe5ed882a1d63c3c280a860b13cde328f86
kakxem <80788785+kakxem@users.noreply.github.com>
2024-10-09 23:01:35 +0200

    fix(import-button): handle player mismatch in imported items
    Update ImportButton to overwrite createdBy with current player ID if original player not in game, ensuring imported items are associate

--- src/components/import-button.tsx
+++ src/components/import-button.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { wsAtom } from "@/hooks/useGame";
-import { Messages } from "@/types";
+import { gameAtom, wsAtom } from "@/hooks/useGame";
+import { Messages, type Item } from "@/types";
 import { useAtomValue } from "jotai";
 import { FileDown } from "lucide-react";
 export const ImportButton = () => {
   const ws = useAtomValue(wsAtom);
+  const game = useAtomValue(gameAtom);
   const handleImportList = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
 
@@ -22,11 +23,29 @@ export const ImportButton = () => {
         if (!reader.result) return;
         const data = JSON.parse(reader.result as string);
         if (!data) return;
+
+        // overwrite 'createdBy' in case the player is not in the game
+        const player = localStorage.getItem("player");
+        if (!player) return;
+        const { id } = JSON.parse(player);
+
+        const parsedData = data.map((item: Item) => {
+          const isPlayerInGame = game.players.some(
+            (player) => player.id === item.createdBy,
+          );
+          if (isPlayerInGame) return item;
+
+          return {
+            ...item,
+            createdBy: id,
+          };
+        });
+
         if (ws) {
           ws.send(
             JSON.stringify({
               type: Messages.IMPORT_LIST,
-              data: { list: data },
+              data: { list: parsedData },
             }),
           );
         }
