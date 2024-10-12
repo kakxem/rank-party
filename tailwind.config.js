f1442678fd7452388f94c641c5d4ec5e006c4605
kakxem <80788785+kakxem@users.noreply.github.com>
2024-10-12 22:21:41 +0200

    fix(fonts): update Baloo2 font implementation
    Move @font-face to global scope, adjust font-family name, add to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -70,6 +70,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        baloo2: ["Baloo2", "sans-serif"],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
